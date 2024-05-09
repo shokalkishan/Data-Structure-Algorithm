c9b22752f661881e01374b007d1930e7966c2f71
Kishan Kumar <shokal812@gmail.com>
2024-05-10 01:45:50 +0530

    subset of sum k

--- dp/knapsack/2_subset_sumk.cpp
+++ dp/knapsack/2_subset_sumk.cpp
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long 
 
 
 // url : https://atcoder.jp/contests/abc321/tasks/abc321_f 
 
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
 
 int32_t main(){
     int q, k ;
     cin >> q >> k;
     vector<int>dp(k+1,0);
     dp[0]=1;
+    int mod = 998244353;
     while(q--){
         char c; cin >> c;
         int x ; cin>>x;
         if(c == '+'){ 
-            for(int i = k-x; i >= 0; i--) dp[i + x] += dp[i];
+            for(int i = k-x; i >= 0; i--) dp[i + x] = (dp[i + x] + dp[i])%mod;
         }
         else{
-            for(int i = 0; i+x <= k; i++) dp[i + x] -= dp[i];
+            for(int i = 0; i+x <= k; i++) dp[i + x] = (dp[i + x] - dp[i] + mod)%mod;
         }
         cout << dp[k] << endl ;
     }
